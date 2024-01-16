3ba89bd44f2b4e35e37c3d8f91d0cf6116fe6d1c
Teguhriyadi <ilham.teguh55@gmail.com>
2024-01-16 22:54:20 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix(['middleware' => ['guest']], function() {
-    Route::prefix('login')->group(function(){
+Route::group(['middleware' => ['guest']], function () {
+    Route::prefix('login')->group(function () {
         Route::get('/', [LoginController::class, 'login']);
         Route::post('/', [LoginController::class, 'post_login']);
     });
 });
 
-Route::prefix(['middleware' => ['is_autentikasi']], function() {
-    Route::prefix('super_admin')->group(function() {
+Route::prefix(['middleware' => ['is_autentikasi']], function () {
+    Route::prefix('super_admin')->group(function () {
         Route::get('/dashboard', [AppController::class, 'dashboard_admin']);
     });
-    Route::prefix('petugas')->group(function() {
+    Route::prefix('petugas')->group(function () {
         Route::get('/dashboard', [AppController::class, 'dashboard_petugas']);
     });
-    Route::prefix('kepala')->group(function() {
+    Route::prefix('kepala')->group(function () {
         Route::get('/dashboard', [AppController::class, 'dashboard_kepala']);
     });
-    Route::prefix('warga')->group(function() {
+    Route::prefix('warga')->group(function () {
         Route::get('/dashboard', [AppController::class, 'dashboard_warga']);
     });
-    
+
     Route::get('/logout', [LoginController::class, 'logout']);
 });
-
-
